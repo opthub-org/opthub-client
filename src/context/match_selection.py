1c1d84ebc2fcbd34dfbc1bde6c6054583922cc36
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2024-03-18 21:44:15 +0900

    🎨 不要なインデントを削除

--- src/context/match_selection.py
+++ src/context/match_selection.py
@@ -6,26 +6,26 @@ class MatchSelectionContext:
         self.competition_id = None
         self.match_id = None
         self.load()
+        
     def load(self):
         if os.path.exists(self.file_path) is not True:
             # file is not found
             self.competition_id = None
             self.match_id = None
             return
-        else:
-            try:
-                with open(self.file_path, 'r') as file:
-                    content = file.read()
-                    parts = content.split(",")
-                    self.competition_id = parts[0]
-                    self.match_id = parts[1]
-            except IOError as e:
-                # file read error
-                print(f"An error occurred while reading the file: {e}. Please select competition and match again" ,file=sys.stderr)
-                self.competition_id = None
-                self.match_id = None
-                return 
-        pass
+        try:
+            with open(self.file_path, 'r') as file:
+                content = file.read()
+                parts = content.split(",")
+                self.competition_id = parts[0]
+                self.match_id = parts[1]
+        except IOError as e:
+            # file read error
+            print(f"An error occurred while reading the file: {e}. Please select competition and match again" ,file=sys.stderr)
+            self.competition_id = None
+            self.match_id = None
+            return 
+        
     def update(self,competition_id, match_id):
         self.competition_id = competition_id
         self.match_id = match_id
