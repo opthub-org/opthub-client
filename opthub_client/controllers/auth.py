cdd34c8990299ecb0d92024561a38f528b00680b
tkumamoto <kumamoto.t.ab@m.titech.ac.jp>
2024-04-09 09:41:58 +0900

    :sparkles: mailaddress and password input

--- opthub_client/controllers/auth.py
+++ opthub_client/controllers/auth.py
@@ -4,6 +4,8 @@ import click
 
 
 @click.command()
+@click.option("--mailaddress", "-m", "mail_address", required=True, prompt=True)
+@click.option("--password", "-p", "password", prompt=True, hide_input=True)
 @click.pass_context
-def auth(ctx: click.Context) -> None:
+def auth(ctx: click.Context, mail_address: str, password: str) -> None:
     """Sign in."""
