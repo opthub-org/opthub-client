7c0b044f6e561824fc6dbe6f05feb6f1d2cafbc3
tkumamoto <kumamoto.t.ab@m.titech.ac.jp>
2024-04-30 22:23:31 +0900

    :+1: add the cli version message

--- opthub_client/controllers/auth.py
+++ opthub_client/controllers/auth.py
@@ -4,11 +4,7 @@ import boto3
 import click
 
 from opthub_client import __version__
-from opthub_client.context.credentials import Credentials
 from opthub_client.graphql.version_cli import get_messages
-
-CLIENT_ID = "7et20feq5fv89j4k430f7ren7s"
-SECRET_HASH = "nrTpTfTDw72mKzN8AD3q813oAH81HpVNFu9+j9g9bLs="
 
 
 @click.command()
@@ -19,7 +15,5 @@ def auth(ctx: click.Context, username: str, password: str) -> None:
     """Sign in."""
     message = get_messages(__version__)
     if message.label == "Error":
-        click.echo(click.style(message.message, fg="red"))
+        click.echo(click.style(message.message, fg=message.labelColor))
         return
-    credentials = Credentials()
-    credentials.cognito_login(username, password)
--- opthub_client/controllers/submit.py
+++ opthub_client/controllers/submit.py
@@ -44,7 +44,7 @@ def submit(match: str | None, competition: str | None, file: bool) -> None:
     """
     message = get_messages(__version__)
     if message.label == "Error":
-        click.echo(click.style(message.message, fg="red"))
+        click.echo(click.style(message.message, fg=message.labelColor))
         return
     match_selection_context = MatchSelectionContext()
     selected_competition, selected_match = match_selection_context.get_selection(match, competition)
