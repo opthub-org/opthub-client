25ac3555223548f614be86e9b1f6b137f232612a
tkumamoto <kumamoto.t.ab@m.titech.ac.jp>
2024-05-20 10:16:59 +0900

    :sparkles: add credential signature and credential encrypto key

--- opthub_client/context/credentials.py
+++ opthub_client/context/credentials.py
@@ -1,14 +1,21 @@
 """This module contains the class related to credentials."""
 
 import shelve
-import tempfile
 import time
 from pathlib import Path
+from typing import Any
 
 import boto3
 import jwt
+import requests
+from cryptography.fernet import Fernet
+from jwcrypto import jwk
 
 CLIENT_ID = "24nvfsrgbuvu75h4o8oj2c2oek"
+JWKS_URL = "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_1Y69fktA0/.well-known/jwks.json"
+KEY_FILE = Path.home() / ".opthub_client" / "encryption_key"
+ERROR_MESSAGE_FAIL_TO_GET_PUBLIC_KEY = "Failed to get public key."
+OPTHUB_CLIENT_DIR = Path.home() / ".opthub_client"
 
 
 class Credentials:
@@ -23,19 +30,21 @@ class Credentials:
 
     def __init__(self) -> None:
         """Initialize the credentials context with a persistent temporary file."""
-        home_dir = Path.home()
-        opthub_client_dir = home_dir / ".opthub_client"
+        opthub_client_dir = OPTHUB_CLIENT_DIR
         opthub_client_dir.mkdir(exist_ok=True)  # Create the directory if it doesn't exist
         self.file_path = opthub_client_dir / "opthub_credentials"
 
     def load(self) -> None:
+        encryption_key = self.load_or_generate_key()
+        cipher_suite = Fernet(encryption_key)
         """Load the credentials from the shelve file."""
         with shelve.open(str(self.file_path)) as db:
-            self.access_token = db.get("access_token", str)
-            self.refresh_token = db.get("refresh_token", str)
-            self.expire_at = db.get("expire_at", str)
-            self.uid = db.get("uid", str)
-            self.username = db.get("username", str)
+            # decrypt the credentials
+            self.access_token = cipher_suite.decrypt(db.get("access_token", b"")).decode()
+            self.refresh_token = cipher_suite.decrypt(db.get("refresh_token", b"")).decode()
+            self.expire_at = cipher_suite.decrypt(db.get("expire_at", b"")).decode()
+            self.uid = cipher_suite.decrypt(db.get("uid", b"")).decode()
+            self.username = cipher_suite.decrypt(db.get("username", b"")).decode()
             # refresh the access token if it is expired
             if self.is_expired() and not self.refresh_access_token():
                 self.clear_credentials()
@@ -44,14 +53,18 @@ class Credentials:
 
     def update(self) -> None:
         """Update the credentials in the shelve file."""
+        encryption_key = self.load_or_generate_key()
+        cipher_suite = Fernet(encryption_key)
         with shelve.open(str(self.file_path)) as db:
-            db["access_token"] = self.access_token
-            db["refresh_token"] = self.refresh_token
+            # encrypt the credentials
+            db["access_token"] = cipher_suite.encrypt(self.access_token.encode())
+            db["refresh_token"] = cipher_suite.encrypt(self.refresh_token.encode())
             # decode the access token to get the expire time, user id and user name
-            token = jwt.decode(self.access_token, options={"verify_signature": False})
-            db["expire_at"] = token.get("exp")
-            db["uid"] = token.get("sub")
-            db["username"] = token.get("username")
+            public_key = self.get_jwks_public_key(self.access_token)
+            token = jwt.decode(self.access_token, public_key, algorithms=["RS256"], options={"verify_signature": True})
+            db["expire_at"] = cipher_suite.encrypt(str(token.get("exp")).encode())
+            db["uid"] = cipher_suite.encrypt(token.get("sub").encode())
+            db["username"] = cipher_suite.encrypt(token.get("username").encode())
             db.sync()
 
     def is_expired(self) -> bool:
@@ -76,7 +89,10 @@ class Credentials:
                 ClientId=CLIENT_ID,
             )
             self.access_token = response["AuthenticationResult"]["AccessToken"]
-            self.expire_at = jwt.decode(self.access_token, options={"verify_signature": False})["exp"]
+            public_key = self.get_jwks_public_key(self.access_token)
+            self.expire_at = jwt.decode(
+                self.access_token, public_key, algorithms=["RS256"], options={"verify_signature": True}
+            )["exp"]
             return True
         except Exception as e:
             return False
@@ -102,14 +118,37 @@ class Credentials:
     def clear_credentials(self) -> None:
         """Clear the credentials in the shelve file."""
         with shelve.open(str(self.file_path)) as db:
-            db["access_token"] = ""
-            db["refresh_token"] = ""
-            db["expire_at"] = ""
-            db["uid"] = ""
-            db["username"] = ""
+            db.clear()
             db.sync()
         self.access_token = ""
         self.refresh_token = ""
         self.expire_at = ""
         self.uid = ""
         self.username = ""
+
+    def get_jwks_public_key(self, access_token: str) -> Any:
+        """Get the public key from the JWKS URL."""
+        response = requests.get(JWKS_URL)
+        jwks = response.json()
+        headers = jwt.get_unverified_header(access_token)
+        kid = headers["kid"]
+
+        public_key_pem = None
+        for key in jwks["keys"]:
+            if key["kid"] == kid:
+                jwk_obj = jwk.JWK(**key)
+                public_key_pem = jwk_obj.export_to_pem()
+                break
+        if public_key_pem is None:
+            raise ValueError(ERROR_MESSAGE_FAIL_TO_GET_PUBLIC_KEY)
+        return public_key_pem
+
+    def load_or_generate_key(self) -> bytes:
+        """Load the encryption key from the shelve file, or generate a new one if it doesn't exist."""
+        with shelve.open(str(self.file_path)) as db:
+            key = db.get("encryption_key")
+            if key is None:
+                key = Fernet.generate_key()
+                db["encryption_key"] = key
+                db.sync()
+            return key
