b5a9e8f2003c104da0c0123f1bbf66644865b335
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2024-07-15 16:18:47 +0900

    feat: check_current_version_status for login and logout

--- opthub_client/controllers/login.py
+++ opthub_client/controllers/login.py
@@ -3,6 +3,7 @@
 import click
 
 from opthub_client.context.credentials import Credentials
+from opthub_client.controllers.utils import check_current_version_status
 from opthub_client.errors.authentication_error import AuthenticationError
 from opthub_client.errors.cache_io_error import CacheIOError
 from opthub_client.errors.fetch_error import FetchError
@@ -19,5 +20,6 @@ def login(ctx: click.Context, username: str, password: str) -> None:  # noqa: AR
         credentials = Credentials()
         credentials.cognito_login(username, password)
         click.echo(f"Hello {username}. Successfully logged in.")
+        check_current_version_status()
     except (CacheIOError, AuthenticationError, QueryError, FetchError) as e:
         e.handler()
--- opthub_client/controllers/logout.py
+++ opthub_client/controllers/logout.py
@@ -3,6 +3,7 @@
 import click
 
 from opthub_client.context.credentials import Credentials
+from opthub_client.controllers.utils import check_current_version_status
 from opthub_client.errors.authentication_error import AuthenticationError
 from opthub_client.errors.cache_io_error import CacheIOError
 from opthub_client.errors.fetch_error import FetchError
@@ -14,6 +15,7 @@ from opthub_client.errors.query_error import QueryError
 def logout(ctx: click.Context) -> None:  # noqa: ARG001
     """Logout."""
     try:
+        check_current_version_status()
         credentials = Credentials()
         credentials.clear_credentials()
         click.echo(" Successfully logged out.")
