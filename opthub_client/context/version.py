9b3d005ee3c28c2742f9bf416e514e7c56eb00ad
tkumamoto <kumamoto.t.ab@m.titech.ac.jp>
2024-08-11 18:33:16 +0900

    fix: revise read version

--- opthub_client/context/version.py
+++ opthub_client/context/version.py
@@ -1,8 +1,6 @@
 """Read version from init.py file."""
 
-from pathlib import Path
-
-from opthub_client.errors.cache_io_error import CacheIOError, CacheIOErrorMessage
+from opthub_client.__init__ import __version__
 
 
 def get_version_from_init() -> str:
@@ -14,16 +12,5 @@ def get_version_from_init() -> str:
     Raises:
         VersionNotFoundError: If the version cannot be found or is in an incorrect format.
     """
-    path = Path("opthub_client/__init__.py")
-    if not path.is_file():
-        raise CacheIOError(CacheIOErrorMessage.VERSION_FILE_READ_FAILED)
-    try:
-        with path.open("r", encoding="utf-8") as file:
-            for line in file:
-                if line.startswith("__version__"):
-                    version = line.split("=")[1].strip().strip('"')
-                    if version:
-                        return version
-        raise CacheIOError(CacheIOErrorMessage.VERSION_FILE_READ_FAILED)
-    except Exception as e:
-        raise CacheIOError(CacheIOErrorMessage.VERSION_FILE_READ_FAILED) from e
+    version = __version__
+    return version
