5a0ba958440708d4f5691c4bd5e13743df2ee186
Lane <lanexpr@outlook.jp>
2024-08-20 19:08:51 +0900

    :add: Initial setup to wrap the 'opthub_api_client'.
    Add 'opthub_api_client' as a dependency in Poetry.

---
+++ opthub_client/api.py
@@ -0,0 +1,11 @@
+"""Access to the OptHub public REST API.
+
+The module is designed to wrap a library that is automatically generated from the OpenAPI schema
+to access the OptHub public REST API.
+
+The automatically generated raw Python package is available at https://github.com/opthub-org/opthub-api-client-python.
+"""
+
+import opthub_api_client
+
+__all__ = ["opthub_api_client"]
