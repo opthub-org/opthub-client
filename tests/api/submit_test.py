c21e61e34f07a0deb0148fa9899eab37c24c88aa
Lane <lanexpr@outlook.jp>
2024-09-06 16:40:26 +0900

    feat: Add a client library for the OptHub public REST API. (#141)

---
+++ tests/api/submit_test.py
@@ -0,0 +1,15 @@
+"""Solution submission test for Public REST API wrapper."""
+
+import tests.api._common as common
+from opthub_client.api import OptHub
+
+EXPECTED_SCORE = 10.0
+
+
+def test_submit() -> None:
+    """Test for submission of the solution."""
+    with OptHub(common.TEST_API_KEY) as api:
+        match = api.match(common.TEST_MATCH)
+        submit = match.submit([1.2, 3.4])
+        score = submit.wait_scoring(timeout=30)
+        assert score.value == EXPECTED_SCORE  # noqa: S101
